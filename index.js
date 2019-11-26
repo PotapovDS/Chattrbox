61f3b516f4914faca510116a3bc5ca0d94b0ecfb
DecaDenZ <potapovdenis09@gmail.com>
2019-11-26 20:36:29 +0300

    файл 404 для ошибки

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 var extract = require('./extract');
 
 var handleError = function (err, res) {
-   res.writehead(404);
+   res.writeHead(404);
    res.end();
 };
 
@@ -11,9 +11,11 @@ var server = http.createServer((req, res) => {
    console.log('Responding to a request.');
    var filePath = extract(req.url);
    fs.readFile(filePath, function (err, data){
+     console.log(data);
       if (err) {
          handleError(err, res);
          return;
+         // filePath = '404';
       } else {
          res.end(data);
       }
