3b7945e5b60a12be2fb35048e3cbdd41919ca51b
DecaDenZ <potapovdenis09@gmail.com>
2019-12-05 13:46:55 +0300

    чат бот авторизован по умолчанию

--- src/chatbot.js
+++ src/chatbot.js
@@ -2,6 +2,8 @@
 var WebSocket = require('ws');
 var chatBot = new WebSocket('http://localhost:3001');
 
+chatBot.isAuthorized = true;
+
 chatBot.hello = function (socket){
    socket.send("Hello, i'm chat-bot Robo!" );
 };
