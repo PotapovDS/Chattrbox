1ee1374b3d779132f6a30b28fa47a22615f3e149
DecaDenZ <potapovdenis09@gmail.com>
2019-12-05 12:33:57 +0300

    добавлена авторизация нового пользователя по паролю

--- websockets-server.js
+++ websockets-server.js
@@ -6,26 +6,50 @@ var ws = new WebSocketServer({
    port: port
 });
 
-var password = 'swordfish';
+var password = 'swordfish'; // общий пароль доступа к чату
 var messages = []; // хранилище сообщений
 
 console.log('websockets server started');
+
+function sendMessageArchive(socket) { //рассылаем архив сообщений новому подключению
+   messages.forEach((msg) => {
+      socket.send(msg);
+   });
+};
 
 ws.on('connection', (socket) => {
    console.log('client connection established');
    socket.send('enter the password');
 
-   messages.forEach((msg) => {  //рассылаем архив сообщений новому подключению
-      socket.send(msg);
+   if (socket.isAuthorized) {
+      sendMessageArchive(socket);
+   };
+
+   // эхо сервер
+   socket.on('message', (data) => { // при получении сообщения добавляем его в хранилище
+
+      if (!socket.isAuthorized) {
+         if (data === password) {
+            socket.isAuthorized = true;
+            socket.send('Welcome to CHATTRBOX!');
+            sendMessageArchive(socket);
+         } else {
+            socket.send('you are not Authorized, please enter the password:');
+         };
+      } else {
+         console.log('message received: ' + data);
+         messages.push(data);
+         ws.clients.forEach((clientSocket) => {
+            if (clientSocket.isAuthorized) {
+               clientSocket.send(data);
+            };
+         });
+      };
+
    });
 
-// эхо сервер
-   socket.on('message', (data) => {  // при получении сообщения добавляем его в хранилище
-      console.log('message received: ' + data);
-      messages.push(data);
-      ws.clients.forEach((clientSocket) => {
-         clientSocket.send(data);
-      });
+});
 
-   });
+ws.on('close', (event) => {
+   console.log(event.reason);
 });
