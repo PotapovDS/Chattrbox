fed24e720b2a0431069b6af440d56b0fa2cfcacd
DecaDenZ <potapovdenis09@gmail.com>
2019-12-12 16:57:43 +0300

    установка jquery в зависимости, создан класс chatform, для обработки событий формы

---
+++ app/scripts/src/dom.js
@@ -0,0 +1,21 @@
+'use strict';
+
+import $ from 'jquery';
+
+export class ChatForm {
+   constructor(formSel, inputSel) {
+      this.$form = $(formSel);
+      this.$input = $(inputSel);
+   }
+
+   init(submitCallback) {
+      this.$form.submit((event) => {
+         event.preventDefault();
+         let val = this.$input.val();
+         submitCallback(val);
+         this.$input.val('');
+      });
+
+      this.$form.find('button').on('click', () => this.$form.submit());
+   }
+}
